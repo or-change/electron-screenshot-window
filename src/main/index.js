7f29c09e33dc234a1ad7ea28cb292535b334c707
DameNeko <phoensxar@gmail.com>
2019-05-06 01:33:23 +0800

    [FIXED] listener

--- src/main/index.js
+++ src/main/index.js
@@ -2,6 +2,15 @@ const path = require('path');
 const { format: formatUrl } = require('url');
 const { BrowserWindow, ipcMain } = require('electron');
 
+const url = formatUrl({
+	pathname: path.resolve(__dirname, '../renderer', 'index.html'),
+	protocol: 'file',
+	slashes: true
+});
+
+// const url = `http://localhost:${process.env.ELECTRON_WEBPACK_WDS_PORT}`;
+
+
 module.exports = async function getRect(dataURL, size) {
 	return new Promise(resolve => {
 		const win = new BrowserWindow({
@@ -16,23 +25,34 @@ module.exports = async function getRect(dataURL, size) {
 				webSecurity: false
 			}
 		});
-	
-		const url = formatUrl({
-			pathname: path.resolve(__dirname, '../renderer', 'index.html'),
-			protocol: 'file',
-			slashes: true
-		});
-
-		// const url = `http://localhost:${process.env.ELECTRON_WEBPACK_WDS_PORT}`;
-
-		win.loadURL(url);
+		
 		win.setBounds({ x: 0, y: 0 });
 
-		ipcMain.on('ELECTRON_SCREENSHOT_WINDOW::get-background', () => {
-			win.webContents.send('ELECTRON_SCREENSHOT_WINDOW::get-background-result', dataURL);
-		});
-		ipcMain.once('ELECTRON_SCREENSHOT_WINDOW::exit', () => win.close());
-	
-		ipcMain.on('ELECTRON_SCREENSHOT_WINDOW::rect-result', (event, args) => win.destroy() & resolve(args));
+		function destroy() {
+			win.destroy();
+			removeAllListener();
+		}
+
+		function removeAllListener() {
+			eventListenerList.forEach(([eventType, listener]) => ipcMain.removeListener(eventType, listener));
+		}
+
+		function addAllListener() {
+			eventListenerList.forEach(([eventType, listener]) => ipcMain.on(eventType, listener));
+		}
+		
+		const onGetBackground = () => win.webContents.send('ELECTRON_SCREENSHOT_WINDOW::get-background-result', dataURL);
+		const onExit = () => destroy();
+		const onRectResult = (event, args) => destroy() & resolve(args);
+
+		const eventListenerList = [
+			['ELECTRON_SCREENSHOT_WINDOW::get-background', onGetBackground],
+			['ELECTRON_SCREENSHOT_WINDOW::exit', onExit],
+			['ELECTRON_SCREENSHOT_WINDOW::rect-result', onRectResult]
+		];
+		
+		addAllListener();
+
+		win.loadURL(url);
 	});
 };
